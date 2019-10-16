bf22a861ca982b60a791afecdd628bf5220daafe
tangweikun <819573105@qq.com>
2019-10-16 23:23:30 +0800

    add moment

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { get } from 'lodash';
-import logo from './logo.svg';
 import './App.css';
 import { getRequest } from './services/request';
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [data, setData] = useState({});
 
   useEffect(() => {
     getMovies();
@@ -16,7 +16,7 @@ function App() {
       const data = get(res, 'data');
       const list = get(data, 'list', []);
       setMovies(list);
-      console.log(list, data);
+      setData(data);
     }
   }, []);
 
@@ -25,71 +25,159 @@ function App() {
       <div className="dashboard-page">
         <div className="dashboard">
           <div className="dashboard-title">
-            <span class="dashboard-nav">
-              <span class="active">综合票房</span>
-              <span class="">分账票房</span>
+            <span className="dashboard-nav">
+              <span className="active">综合票房</span>
+              <span className="">分账票房</span>
             </span>
-            <div class="dashboard-title-clock">
-              <span class="sprite sprite-logo"></span>
-              <span class="clock">
+            <div className="dashboard-title-clock">
+              <span className="sprite sprite-logo"></span>
+              <span className="clock">
                 <span>
-                  <span>2019年10月16日</span>&nbsp;<span>22:07</span>
+                  <span>{get(data, 'serverTime')}</span>&nbsp;<span>22:07</span>
                 </span>
               </span>
-            </div>
-            <div class="dashboard-title-group">
-              <div class="dashboard-title-togglefullscreen button">全屏</div>
-              <div class="dashboard-title-link button">
-                <a href="/?ver=normal">
-                  返回常规版<span class="sprite sprite-arrow"></span>
-                </a>
-              </div>
             </div>
           </div>
 
           <div className="dashboard-content">
-            <div class="dashboard-left">
-              <div class="dashboard-detail bg">
-                <div class="detail-part-name">
-                  <p class="detail-title">
-                    <span class="sprite sprite-star"></span>
+            <div className="dashboard-left">
+              <div className="dashboard-detail bg">
+                <div className="detail-part-name">
+                  <p className="detail-title">
+                    <span className="sprite sprite-star"></span>
                     <span>打过长江去</span>
                   </p>
-                  <p class="detail-releaseinfo">上映首日&nbsp;232.2万</p>
+                  <p className="detail-releaseinfo">上映首日&nbsp;232.2万</p>
                 </div>
-                <div class="detail-part-box">
-                  <div class="detail-box">
-                    <p class="detail-realtime-desc">综合票房</p>
-                    <p class="detail-realtime">232.23万</p>
-                    <p class="detail-realtime-ratio">票房占比2.4%</p>
+                <div className="detail-part-box">
+                  <div className="detail-box">
+                    <p className="detail-realtime-desc">综合票房</p>
+                    <p className="detail-realtime">232.23万</p>
+                    <p className="detail-realtime-ratio">票房占比2.4%</p>
                   </div>
                 </div>
-                <div class="detail-part-extra">
-                  <div class="detail-item">
-                    <p class="detail-item-title">排片场次</p>
-                    <p class="detail-item-num">5648</p>
-                    <p class="detail-item-ratio">排片占比1.7%</p>
+                <div className="detail-part-extra">
+                  <div className="detail-item">
+                    <p className="detail-item-title">排片场次</p>
+                    <p className="detail-item-num">5648</p>
+                    <p className="detail-item-ratio">排片占比1.7%</p>
                   </div>
-                  <div class="detail-item">
-                    <p class="detail-item-title">场均人次</p>
-                    <p class="detail-item-num">13</p>
-                    <p class="detail-item-ratio">上座率11.6%</p>
+                  <div className="detail-item">
+                    <p className="detail-item-title">场均人次</p>
+                    <p className="detail-item-num">13</p>
+                    <p className="detail-item-ratio">上座率11.6%</p>
                   </div>
                 </div>
               </div>
-              <div class="dashboard-push"></div>
-              <div class="dashboard-calendar-container bg">
-                <div class="dashboard-calendar">
-                  <div class="cal-current">
-                    2019.10.16<span class="traingle"></span>
-                  </div>
-                  <div class="cal-box">
+              <div className="dashboard-push"></div>
+              <div className="dashboard-calendar-container bg">
+                <div className="dashboard-calendar">
+                  <div className="cal-box">
                     <span>今日实时</span>
-                    <span class="cal-box-num">9334.9万</span>
+                    <span className="cal-box-num">9334.9万</span>
                   </div>
-                  <p class="cal-update-time">
+                  <p className="cal-update-time">
                     <span>北京时间 22:09:38</span>
                   </p>
+                </div>
+              </div>
+            </div>
+            <div className="dashboard-list bg">
+              <table className="dashboard-table dashboard-table-header">
+                <thead>
+                  <tr>
+                    <th className="moviename-th">
+                      <div>
+                        <span>影片</span>
+                        <span className="mn">
+                          (点击
+                          <span className="sprite sprite-tiny-star"></span>
+                          优先展示)
+                        </span>
+                      </div>
+                    </th>
+                    <th>
+                      <div className="theader">综合票房(万)</div>
+                    </th>
+                    <th>
+                      <div className="theader">票房占比</div>
+                    </th>
+                    <th>
+                      <div className="theader">排片场次</div>
+                    </th>
+                    <th>
+                      <div className="theader">排片占比</div>
+                    </th>
+                    <th>
+                      <div className="theader">场均人次</div>
+                    </th>
+                    <th>
+                      <div className="theader">上座率</div>
+                    </th>
+                  </tr>
+                </thead>
+              </table>
+              <div className="movielist-container">
+                <div className="movielist">
+                  <table className="dashboard-table">
+                    <tbody>
+                      <tr className="selected-movie">
+                        <td className="moviename-td" title="打过长江去">
+                          <div>
+                            <div className="moviename-num">
+                              <p className="moviename-index">05</p>
+                              <p className="moviename-star">
+                                <span className="sprite sprite-star"></span>
+                              </p>
+                            </div>
+                            <div className="moviename-desc">
+                              <p className="moviename-name">打过长江去</p>
+                              <p className="moviename-info">
+                                <span>上映首日</span>&nbsp;&nbsp;
+                                <span>232.7万</span>
+                              </p>
+                            </div>
+                          </div>
+                        </td>
+                        <td className="realtime">232.74</td>
+                        <td>2.4%</td>
+                        <td>5640</td>
+                        <td>1.7%</td>
+                        <td>13</td>
+                        <td>11.6%</td>
+                      </tr>
+
+                      {movies.map((item, index) => (
+                        <tr className="" key={item.movieId}>
+                          <td className="moviename-td" title={item.movieName}>
+                            <div>
+                              <div className="moviename-num">
+                                <p className="moviename-index">{index + 1}</p>
+                                <p className="moviename-star">
+                                  <span className="sprite sprite-star-gray"></span>
+                                </p>
+                              </div>
+                              <div className="moviename-desc">
+                                <p className="moviename-name">
+                                  {item.movieName}
+                                </p>
+                                <p className="moviename-info">
+                                  <span>{item.releaseInfo}</span>&nbsp;&nbsp;
+                                  <span>{item.sumBoxInfo}</span>
+                                </p>
+                              </div>
+                            </div>
+                          </td>
+                          <td className="realtime">{item.boxInfo}</td>
+                          <td>&lt;{item.boxRate}</td>
+                          <td>{item.showInfo}</td>
+                          <td>&lt;{item.showRate}</td>
+                          <td>{item.avgShowView}</td>
+                          <td>{item.seatRate}</td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
                 </div>
               </div>
             </div>
