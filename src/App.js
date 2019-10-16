704822470d6edf69da85adba83cae810294fe440
tangweikun <819573105@qq.com>
2019-10-17 00:29:09 +0800

    make select movie work

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { getRequest } from './services/request';
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState([]);
   const [data, setData] = useState({});
 
   useEffect(() => {
@@ -16,6 +17,7 @@ function App() {
       const data = get(res, 'data');
       const list = get(data, 'list', []);
       setMovies(list);
+      setSelectedMovie(get(list, '0', {}));
       setData(data);
     }
   }, []);
@@ -45,27 +47,37 @@ function App() {
                 <div className="detail-part-name">
                   <p className="detail-title">
                     <span className="sprite sprite-star"></span>
-                    <span>打过长江去</span>
+                    <span>{selectedMovie.movieName}</span>
                   </p>
-                  <p className="detail-releaseinfo">上映首日&nbsp;232.2万</p>
+                  <p className="detail-releaseinfo">
+                    {selectedMovie.releaseInfo}&nbsp;{selectedMovie.sumBoxInfo}
+                  </p>
                 </div>
                 <div className="detail-part-box">
                   <div className="detail-box">
                     <p className="detail-realtime-desc">综合票房</p>
-                    <p className="detail-realtime">232.23万</p>
-                    <p className="detail-realtime-ratio">票房占比2.4%</p>
+                    <p className="detail-realtime">{selectedMovie.boxInfo}万</p>
+                    <p className="detail-realtime-ratio">
+                      票房占比{selectedMovie.boxRate}
+                    </p>
                   </div>
                 </div>
                 <div className="detail-part-extra">
                   <div className="detail-item">
                     <p className="detail-item-title">排片场次</p>
-                    <p className="detail-item-num">5648</p>
-                    <p className="detail-item-ratio">排片占比1.7%</p>
+                    <p className="detail-item-num">{selectedMovie.showInfo}</p>
+                    <p className="detail-item-ratio">
+                      排片占比{selectedMovie.showRate}
+                    </p>
                   </div>
                   <div className="detail-item">
                     <p className="detail-item-title">场均人次</p>
-                    <p className="detail-item-num">13</p>
-                    <p className="detail-item-ratio">上座率11.6%</p>
+                    <p className="detail-item-num">
+                      {selectedMovie.avgShowView}
+                    </p>
+                    <p className="detail-item-ratio">
+                      上座率{selectedMovie.seatRate}
+                    </p>
                   </div>
                 </div>
               </div>
@@ -123,40 +135,30 @@ function App() {
                 <div className="movielist">
                   <table className="dashboard-table">
                     <tbody>
-                      {/* <tr className="selected-movie">
-                        <td className="moviename-td" title="打过长江去">
-                          <div>
-                            <div className="moviename-num">
-                              <p className="moviename-index">05</p>
-                              <p className="moviename-star">
-                                <span className="sprite sprite-star"></span>
-                              </p>
-                            </div>
-                            <div className="moviename-desc">
-                              <p className="moviename-name">打过长江去</p>
-                              <p className="moviename-info">
-                                <span>上映首日</span>&nbsp;&nbsp;
-                                <span>232.7万</span>
-                              </p>
-                            </div>
-                          </div>
-                        </td>
-                        <td className="realtime">232.74</td>
-                        <td>2.4%</td>
-                        <td>5640</td>
-                        <td>1.7%</td>
-                        <td>13</td>
-                        <td>11.6%</td>
-                      </tr> */}
-
                       {movies.map((item, index) => (
-                        <tr className="" key={item.movieId}>
+                        <tr
+                          className={
+                            item.movieId === selectedMovie.movieId
+                              ? 'selected-movie'
+                              : ''
+                          }
+                          key={item.movieId}
+                          onClick={() => {
+                            setSelectedMovie(item);
+                          }}
+                        >
                           <td className="moviename-td" title={item.movieName}>
                             <div>
                               <div className="moviename-num">
                                 <p className="moviename-index">{index + 1}</p>
                                 <p className="moviename-star">
-                                  <span className="sprite sprite-star-gray"></span>
+                                  <span
+                                    className={`sprite ${
+                                      item.movieId === selectedMovie.movieId
+                                        ? 'sprite-star'
+                                        : 'sprite-star-gray'
+                                    }`}
+                                  ></span>
                                 </p>
                               </div>
                               <div className="moviename-desc">
